6cc19484b5462d9cb98873e0b39e90540798c7b3
Pia-N <pianurmi@hotmail.fi>
2021-01-27 13:17:58 +0200

    1.8: unicafe step3

--- part 1/unicafe/my-app/src/index.js
+++ part 1/unicafe/my-app/src/index.js
@@ -1,35 +1,61 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
-
 const App = (props) => {
+  console.log(props, "this is App");
   
   const [good, setGood] = useState(0)
   const voteGood = ()=> setGood(good +1)
+  
 
   const [neutral, setNeutral] = useState(0)
   const voteNeutral =()=>setNeutral (neutral +1)
+  
 
   const [bad, setBad] = useState(0)
   const voteBad =()=>setBad(bad +1)
+  
+
+  const [total]= [good + neutral + bad]
+  const [average] = [((good * 1) + (neutral * 0) + (bad * -1)) / total]
+  const [positive] = [(good / total)* 100]
 
  return (
     <>
-    <h1>Give feedback </h1>
-    <button onClick = {voteGood} > good </button>
-    <button onClick = {voteNeutral} > neutral </button>
-    <button onClick = {voteBad} > bad </button>
-    <h2>Statistics </h2>
-    <ul> 
-      <li>good {good}</li>
-      <li>neutral {neutral}</li>
-      <li>bad {bad}</li>
-    </ul>  
-    </>
+    <h1>Give feedback</h1>
+    < Button handleClick = {voteGood} 
+      text="Good" />
+    < Button handleClick = {voteNeutral} 
+      text= "Neutral" />
+    < Button handleClick = {voteBad}
+      text= "Bad" />
+
+    <h2>Statistics</h2>
+    < Statistics name = "Good" value = {good} />
+    < Statistics name = "Neutral" value = {neutral} />
+    < Statistics name = "Bad" value = {bad} />
+    < Statistics name = "Total" value = {total} />
+    < Statistics name = "Average" value = {average} />
+    < Statistics name = "Positive" value = {positive} mark= "%"/>
+  </>
   )
    
 }
+const Button = (props) => {
+  return (
+    <button onClick = {props.handleClick}>
+      {props.text} 
+    </button>
+  )
+}
+const Statistics = (props)=> {
+  return(
+    <div>
+     {props.name} {props.value} {props.mark}
+   </div>
+  )
+}
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
