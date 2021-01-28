5e7a333256b2bda741da59f92dcd242c1744f7a5
Pia-N <pianurmi@hotmail.fi>
2021-01-28 17:28:17 +0200

    1.10: unicafe step5

--- part 1/unicafe/my-app/src/index.js
+++ part 1/unicafe/my-app/src/index.js
@@ -2,57 +2,74 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
-  console.log(props, "this is App");
   
   const [good, setGood] = useState(0)
-  const voteGood = ()=> setGood(good +1)
-  
-
   const [neutral, setNeutral] = useState(0)
-  const voteNeutral =()=>setNeutral (neutral +1)
-  
-
   const [bad, setBad] = useState(0)
-  const voteBad =()=>setBad(bad +1)
   
 
-  const [total]= [good + neutral + bad]
-  const [average] = [((good * 1) + (neutral * 0) + (bad * -1)) / total]
-  const [positive] = [(good / total)* 100]
+  const voteGood = ()=> setGood(good +1)
+  const voteNeutral =()=>setNeutral (neutral +1)
+  const voteBad =()=>setBad(bad +1)
 
  return (
     <>
-    <h1>Give feedback</h1>
-    < Button handleClick = {voteGood} 
-      text="Good" />
-    < Button handleClick = {voteNeutral} 
-      text= "Neutral" />
-    < Button handleClick = {voteBad}
-      text= "Bad" />
+    < Header headerText1 = "Give feedback" />
+    < Button handleClick = {voteGood} text="Good" />
+    < Button handleClick = {voteNeutral} text= "Neutral" />
+    < Button handleClick = {voteBad} text= "Bad" />
 
-    <h2>Statistics</h2>
-    < Statistics name = "Good" value = {good} />
-    < Statistics name = "Neutral" value = {neutral} />
-    < Statistics name = "Bad" value = {bad} />
-    < Statistics name = "Total" value = {total} />
-    < Statistics name = "Average" value = {average} />
-    < Statistics name = "Positive" value = {positive} mark= "%"/>
-  </>
+     <Header headerText2 = "Statistics" />
+     <Statistics good = {good} neutral = {neutral} bad = {bad} />
+    </>
   )
    
 }
-const Button = (props) => {
+const Header = ({headerText1, headerText2}) => {
   return (
-    <button onClick = {props.handleClick}>
-      {props.text} 
-    </button>
+    <>
+    <h1>{headerText1}</h1>
+    <h2>{headerText2}</h2>
+  </>
   )
 }
-const Statistics = (props)=> {
+const Button = ({handleClick,text}) => {
+  return (
+    <button onClick = {handleClick}>
+      {text} </button>
+  )
+}
+const Statistics = ({good, neutral, bad,})=> {
+  
+  const total = [good + neutral + bad]
+  const average = [((good * 1) + (neutral * 0) + (bad * -1)) / total]
+  const positive = [(good / total)* 100, "%"]
+
+  if(total <= 0){
+  return(
+    <>
+    <p> No feedback given</p>
+    </>
+  )
+}
+  return (
+    <div>
+          <Statistic name = "Good " value = {good} />
+          <Statistic name = "Neutral " value = {neutral} />
+          <Statistic name = "Bad " value = {bad} />
+          <Statistic name = "Total " value = {total} />
+          <Statistic name = "Average " value = {average} />
+          <Statistic name = "Positive " value = {positive} />
+      
+    </div>
+  )
+}
+const Statistic = ({name, value}) => {
   return(
     <div>
-     {props.name} {props.value} {props.mark}
-   </div>
+    <p>{name}{value}</p> 
+    </div>
+
   )
 }
 
