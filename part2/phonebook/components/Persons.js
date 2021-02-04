a35c85d91f5830501dd7283684238d4b6a5f1919
Pia-N <pianurmi@hotmail.fi>
2021-02-04 21:32:09 +0200

    2.6: The Phonebook Step1

---
+++ part2/phonebook/components/Persons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Persons = (props) => {
+    return (
+    <div>
+        {props.name}
+    </div>
+ )
+}
+export default Persons
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id:'' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  //tapahtumakäsittelijä estää sivun uudelleenlataamisen 
+  // sekä luo uuden henkilöolion joka tallennetaan staten kautta lomakkeeseen.
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+ // tämä funktio tarkkailee input-kenttien tilaa ja asettaa uuden arvon muuttujalle
+  // console.log(event.target.value), tämä tulostaa konsoliin mitä syöttökentässä lukee
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  console.log(persons.map, "this is persons map");
+  return (
+  
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit = {addName}>
+        name: <input value = {newName}
+        onChange = {handleNameChange} />
+        <div>
+        <button type="submit">add</button>
+        </div>
+      </form >
+      <h2>Numbers</h2>
+      <ul>
+      {persons.map (person => 
+      <li key = {person.id}>{person.name}</li>
+      )}
+      </ul>
+      
+      ...
+    </div>
+  )
+}
+
+export default App
