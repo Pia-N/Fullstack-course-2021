59247c5b30df8d4699e4baa2ed4f7f1bac5668cf
Pia-N <pianurmi@hotmail.fi>
2021-02-05 11:41:17 +0200

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,9 +4,11 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas',
-      id:'' }
+      number: '0500 0800' }
   ]) 
+  const [newNumber, setNewNumber] = useState('')
   const [ newName, setNewName ] = useState('')
+
   //tapahtumakäsittelijä estää sivun uudelleenlataamisen 
   // sekä luo uuden henkilöolion joka tallennetaan staten kautta lomakkeeseen.
   const addName = (event) => {
@@ -20,18 +22,25 @@ const App = () => {
   else {
     const personObject = {
       name: newName,
-      id: newName
+      id: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     setNewName("")
+    setNewNumber('')
+    }
   }
-    
-}
+ 
+  
  // tämä funktio tarkkailee input-kenttien tilaa ja asettaa uuden arvon muuttujalle
   // console.log(event.target.value), tämä tulostaa konsoliin mitä syöttökentässä lukee
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
   console.log(persons.map, "this is persons map");
   return (
@@ -40,20 +49,21 @@ const App = () => {
       <h2>Phonebook</h2>
 
       <form onSubmit = {addName}>
-        name: <input value = {newName}
-        onChange = {handleNameChange} />
-        <div>
+        <div>name: <input value = {newName}
+        onChange = {handleNameChange} /> 
+       <div>number: <input value = {newNumber} 
+        onChange ={handleNumberChange} />
+        </div>
         <button type="submit">add</button>
         </div>
       </form >
+  
       <h2>Numbers</h2>
       <ul>
       {persons.map (person => 
-      <li key = {person.id}>{person.name}</li>
+      <li key = {person.name}>{person.name} {person.number}</li>
       )}
       </ul>
-      
-      ...
     </div>
   )
 }
