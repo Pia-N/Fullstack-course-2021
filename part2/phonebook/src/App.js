d785f03142a74600283c109ce83165b40b21b870
Pia-N <pianurmi@hotmail.fi>
2021-02-05 13:49:10 +0200

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,11 +3,19 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '0500 0800' }
+    { name: 'Arto Hellas', number: '0500 0800' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    
   ]) 
   const [newNumber, setNewNumber] = useState('')
-  const [ newName, setNewName ] = useState('')
+  const [newName, setNewName ] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const showPersons = newFilter.length === 0
+  ? persons 
+  : persons.filter(person => person.name.toUpperCase().includes(newFilter.toUpperCase()))
+    
 
   //tapahtumakäsittelijä estää sivun uudelleenlataamisen 
   // sekä luo uuden henkilöolion joka tallennetaan staten kautta lomakkeeseen.
@@ -17,7 +25,7 @@ const App = () => {
   if(persons.find(person => person.name === newName))
   {
     window.alert(`${newName} is already added to phonebook`)
-    setNewName("")
+    setNewName('')
   }
   else {
     const personObject = {
@@ -26,10 +34,11 @@ const App = () => {
       number: newNumber
     }
     setPersons(persons.concat(personObject))
-    setNewName("")
+    setNewName('')
     setNewNumber('')
     }
   }
+  
  
   
  // tämä funktio tarkkailee input-kenttien tilaa ja asettaa uuden arvon muuttujalle
@@ -42,12 +51,20 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleFilterChange =(event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
   console.log(persons.map, "this is persons map");
   return (
   
     <div>
       <h2>Phonebook</h2>
-
+      <div>
+      <div>filter: <input value = {newFilter}
+        onChange = {handleFilterChange} /> 
+      </div>  
+      </div> 
       <form onSubmit = {addName}>
         <div>name: <input value = {newName}
         onChange = {handleNameChange} /> 
@@ -60,12 +77,12 @@ const App = () => {
   
       <h2>Numbers</h2>
       <ul>
-      {persons.map (person => 
+      {showPersons.map (person => 
       <li key = {person.name}>{person.name} {person.number}</li>
       )}
       </ul>
     </div>
-  )
+    )
 }
 
 export default App
