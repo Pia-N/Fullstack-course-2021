32f6f6a0f82836b6bd6ece822f7456d09cc901e2
Pia-N <pianurmi@hotmail.fi>
2021-02-27 23:42:46 +0200

    cleaned comments

--- part2/phonebook/src/components/Button.js
+++ part2/phonebook/src/components/Button.js
@@ -1,27 +1,6 @@
 import React from 'react'
 import personService from '../services/personsdata'
 
-/* const handleClick = (props) => { */
-/*   const { id, persons, setPersons, setNotificationType, setErrorMsg } = props
-/*   const ok = window.confirm("Do you really want to delete your information?")
-/*   console.log("nappi delete"); */
-/*   console.log(id, "id"); */
-/*   console.log(setPersons, "uusi tila"); */
-/*   console.log(persons, "tyypit"); */
-/*  */
-/*   if (ok) { */
-/*     personService.remove(id) */
-/*       .then(() => { */
-/*         setPersons(persons.filter(person => person.id !== id)) */
-/*         setErrorMsg(`'Information has been deleted`) */
-/*         setNotificationType('error') */
-/*         setTimeout(() => { */
-/*           setErrorMsg(null) */
-/*         }, 5000) */
-/*       }) */
-/*  */
-/*   } */
-/* } */
 
 
 const deletePerson = (props) => {
@@ -44,22 +23,13 @@ const deletePerson = (props) => {
   }
 }
 
-
-
 const Button = (props) => {
 
   return (
     <button onClick={() => deletePerson(props)}>Delete</button>
   )
 }
-/*const deletePerson = (props) => {
-  console.log(props, "this is deletePerson props")
-}
-const Button = (props) => {
-  return (
-    <button onClick={() => deletePerson(props)}>Delete</button>
-  )
-}*/
+
 
 export default Button
 
